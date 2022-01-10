5571f5505ca5a2ba53b550d1acbba3631cfe712b
Mike Mallon <mallon127@gmail.com>
2022-01-10 11:04:31 -0600

    display and delete user's posts

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,26 +1,3 @@
-const newFormHandler = async (event) => {
-  event.preventDefault();
-
-  const name = document.querySelector('#post-name').value.trim();
-  //const needed_funding = document.querySelector('#post-funding').value.trim();
-  const description = document.querySelector('#post-desc').value.trim();
-
-  if (name && description) {
-    const response = await fetch(`/api/posts`, {
-      method: 'POST',
-      body: JSON.stringify({ name, description }),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
-
-    if (response.ok) {
-      document.location.replace('/dashboard');
-    } else {
-      alert('Failed to create post');
-    }
-  }
-};
 
 const delButtonHandler = async (event) => {
   if (event.target.hasAttribute('data-id')) {
@@ -37,10 +14,6 @@ const delButtonHandler = async (event) => {
     }
   }
 };
-
-document
-  .querySelector('.new-post-form')
-  .addEventListener('submit', newFormHandler);
 
 document
   .querySelector('.post-list')
